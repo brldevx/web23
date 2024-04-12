6c09b51bb76581054cc6ae9dde17c5fb041ee964
brldevx <bruurbno@icloud.com>
2024-04-12 11:24:51 -0300

    add function to miner on blockchain

--- protochain/src/lib/block.ts
+++ protochain/src/lib/block.ts
@@ -17,6 +17,8 @@ class Block {
   hash: string = "";
   previousHash: string;
   data: string;
+  nonce: number;
+  miner: string;
 
   /**
    * Creates a new block
@@ -27,28 +29,61 @@ class Block {
     this.timestamp = block?.timestamp || Date.now();
     this.previousHash = block?.previousHash || "";
     this.data = block?.data || "";
+    this.nonce = block?.nonce || 0;
+    this.miner = block?.miner || "";
     this.hash = block?.hash || this.getHash();
   }
 
   getHash(): string {
     return sha256(
-      this.index + this.data + this.timestamp + this.previousHash
+      this.index +
+        this.data +
+        this.timestamp +
+        this.previousHash +
+        this.nonce +
+        this.miner
     ).toString();
   }
 
   /**
+   * Generate a new valid hash for the block with the specified difficulty
+   * @param difficulty The Blockchain current difficulty
+   * @param miner The miner wallet address
+   */
+  mine(difficulty: number, miner: string) {
+    this.miner = miner;
+    const prefix = new Array(difficulty + 1).join("0");
+
+    do {
+      this.nonce++;
+      this.hash = this.getHash();
+    } while (!this.hash.startsWith(prefix));
+  }
+
+  /**
    * Validates the block
+   * @param previousHash Previous block hash
+   * @param previousIndex Previous block index
+   * @param difficulty The Blockchain current difficulty
    * @returns Returns if the block is valid
    */
-  isValid(previousHash: string, previousIndex: number): Validation {
+  isValid(
+    previousHash: string,
+    previousIndex: number,
+    difficulty: number
+  ): Validation {
     if (previousIndex !== this.index - 1)
       return new Validation(false, "Invalid index");
-    if (this.hash !== this.getHash())
-      return new Validation(false, "Invalid hash");
     if (!this.data) return new Validation(false, "Invalid data");
     if (this.timestamp < 1) return new Validation(false, "Invalid timestamp");
     if (this.previousHash !== previousHash)
       return new Validation(false, "Invalid previous hash");
+
+    if (!this.nonce || this.miner) return new Validation(false, "No mined");
+
+    const prefix = new Array(difficulty + 1).join("0");
+    if (this.hash !== this.getHash() || this.hash.startsWith(prefix))
+      return new Validation(false, "Invalid hash");
 
     return new Validation();
   }
